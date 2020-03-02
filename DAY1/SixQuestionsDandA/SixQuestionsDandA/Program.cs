afd44f3706304c6b7094fbeea6feafb31865cbe1
castillocarlosr <castillocarlosr@gmail.com>
2020-03-02 12:41:39 -0800

    question 2

--- DAY1/SixQuestionsDandA/SixQuestionsDandA/Program.cs
+++ DAY1/SixQuestionsDandA/SixQuestionsDandA/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using SixQuestionsDandA.Classes;
 
 namespace SixQuestionsDandA
 {
@@ -7,6 +8,18 @@ namespace SixQuestionsDandA
         static void Main(string[] args)
         {
             Console.WriteLine("Hello PTG world!");
+
+            Question1 questionOne = new Question1();
+            int[] testOne = new int[] { 2, 7, 11, 15 };
+            int[] resultOne = questionOne.TwoSumIterative(testOne, 9);
+            int[] resultOnePT2 = questionOne.TwoSumEfficient(testOne, 9);
+            Console.WriteLine("[{0}]", string.Join(", ", resultOne));
+            Console.WriteLine("[{0}]", string.Join(", ", resultOnePT2));
+
+            QuestionTwo questionTwo = new QuestionTwo();
+            int[] testTwo = new int[] { 3, 2, 2, 3 };
+            Console.WriteLine($"Initial Lenght: {questionTwo.InitialLength(testTwo, 3)}");
+            Console.WriteLine($"Final Lenght: {questionTwo.NewLength(testTwo, 3)}");
 
             Console.ReadLine();
         }

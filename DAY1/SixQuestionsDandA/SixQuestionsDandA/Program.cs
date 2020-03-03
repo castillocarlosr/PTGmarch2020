6c240aceec9f727750dd8dca4d3b689ebd94c24c
castillocarlosr <castillocarlosr@gmail.com>
2020-03-02 16:59:58 -0800

    all six questions

--- DAY1/SixQuestionsDandA/SixQuestionsDandA/Program.cs
+++ DAY1/SixQuestionsDandA/SixQuestionsDandA/Program.cs
@@ -24,9 +24,20 @@ namespace SixQuestionsDandA
             Question3 question3 = new Question3();
             int[] test3Arr1 = new int[] { 1, 2, 3, 0, 0, 0 };
             int[] test3Arr2 = new int[] { 2, 5, 6 };
-            int[] question3Result = question3.MergeSort(test3Arr1, test3Arr2);
+            //int[] question3Result = question3.MergeSort(test3Arr1, test3Arr2);
             Console.WriteLine("Question 3");
-            Console.WriteLine("[{0}]", string.Join(", ", question3Result));
+            //Console.WriteLine("[{0}]", string.Join(", ", question3Result));
+
+            Question4 question4 = new Question4();
+
+            Question5 question5 = new Question5();
+
+
+            Question6 question6 = new Question6();
+            string test6True = "(){}[]{{}}[[]]({()})";
+            string test6False = "((()(){{{{}}{{[[]";
+            Console.WriteLine($"True should be: {question6.CharterValidation(test6True)}");
+            Console.WriteLine($"False should be: {question6.CharterValidation(test6False)}");
 
             Console.ReadLine();
         }

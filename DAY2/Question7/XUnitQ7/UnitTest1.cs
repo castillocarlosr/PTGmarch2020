e38a73ef414591863abac390b10f6dd093851b49
castillocarlosr <castillocarlosr@gmail.com>
2020-03-03 10:52:16 -0800

    added xunit

--- DAY2/Question7/XUnitQ7/UnitTest1.cs
+++ DAY2/Question7/XUnitQ7/UnitTest1.cs
@@ -1,5 +1,6 @@
 using System;
 using Xunit;
+using Question7.Classes;
 
 namespace XUnitQ7
 {
@@ -8,7 +9,30 @@ namespace XUnitQ7
         [Fact]
         public void Test1()
         {
+            QuestionSeven questionSeven = new QuestionSeven();
 
+            Assert.Equal("five", questionSeven.NumbersToWords(5));
+        }
+        [Fact]
+        public void Test2()
+        {
+            QuestionSeven questionSeven = new QuestionSeven();
+
+            Assert.Equal("ninehundred and forty-three", questionSeven.NumbersToWords(943));
+        }
+        [Fact]
+        public void TestNegative()
+        {
+            QuestionSeven questionSeven = new QuestionSeven();
+
+            Assert.Equal("negative seventy-two", questionSeven.NumbersToWords(-72));
+        }
+        [Fact]
+        public void TestBig()
+        {
+            QuestionSeven questionSeven = new QuestionSeven();
+
+            Assert.Equal("fivehundred and forty-threethousand ninehundred and eighty-seven", questionSeven.NumbersToWords(543987));
         }
     }
 }

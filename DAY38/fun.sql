9cd40b9a4955140ac8895628b991bf7b0bec7bb2
CARLOS R. CASTILLO <castillocarlosr@gmail.com>
2020-04-07 13:23:01 -0700

    added all 4 tables

--- DAY38/fun.sql
+++ DAY38/fun.sql
@@ -2,7 +2,11 @@
 -- Drop the table if it already exists
 IF OBJECT_ID('SchemaName.TableName', 'U') IS NOT NULL
 DROP TABLE duringCovid.card_rank
+DROP TABLE duringCovid.card_suit
+DROP TABLE duringCovid.games
+DROP TABLE duringCovid.inventory
 GO
+
 -- Create the table in the specified schema
 CREATE TABLE duringCovid.card_rank
 (
@@ -11,4 +15,25 @@ CREATE TABLE duringCovid.card_rank
     Column2 [NVARCHAR](50) NOT NULL
     -- specify more columns here
 );
+CREATE TABLE duringCovid.card_suit
+(
+    TableNameId INT NOT NULL PRIMARY KEY, -- primary key column
+    Column1 [NVARCHAR](50) NOT NULL,
+    Column2 [NVARCHAR](50) NOT NULL
+    -- specify more columns here
+);
+CREATE TABLE duringCovid.games
+(
+    TableNameId INT NOT NULL PRIMARY KEY, -- primary key column
+    Column1 [NVARCHAR](50) NOT NULL,
+    Column2 [NVARCHAR](50) NOT NULL
+    -- specify more columns here
+);
+CREATE TABLE duringCovid.inventory
+(
+    TableNameId INT NOT NULL PRIMARY KEY, -- primary key column
+    Column1 [NVARCHAR](50) NOT NULL,
+    Column2 [NVARCHAR](50) NOT NULL
+    -- specify more columns here
+);    
 GO
